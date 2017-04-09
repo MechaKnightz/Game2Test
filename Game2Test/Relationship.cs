afacb34f79581c2b3d6cac241e9f0e9421a7c3ef
MechaKnightz <MechaKnightz@hotmail.com>
2017-04-09 20:35:56 +0200

    added relations enum

---
+++ Game2Test/Relationship.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2Test
+{
+    enum Relationship
+    {
+        Ally,
+        Friendly,
+        Neutral,
+        Dislike,
+        Enemy
+    }
+}

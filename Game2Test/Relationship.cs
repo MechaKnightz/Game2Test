0bc15dfb86ec4a64b7a2dea907ea541ed9626593
MechaKnightz <MechaKnightz@hotmail.com>
2017-04-10 04:58:29 +0200

    cleaned using statements from enum

--- Game2Test/Relationship.cs
+++ Game2Test/Relationship.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Game2Test
+﻿namespace Game2Test
 {
     enum Relationship
     {

0144fae10f36698f08211ece26530a45fa50199c
MechaKnightz <MechaKnightz@hotmail.com>
2017-04-12 05:17:38 +0200

    added tractorbeam class, not used anywhere yet though

---
+++ Game2Test/Sprites/Entities/TractorBeam.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game2Test.Sprites.Entities
+{
+    public class TractorBeam : Sprite
+    {
+        public float Length { get; set; }
+
+        public TractorBeam(TractorBeam tractorBeam)
+        {
+            Length = tractorBeam.Length;
+            //Sprite
+            Rotation = tractorBeam.Rotation;
+            Position = tractorBeam.Position;
+            Rectangle = tractorBeam.Rectangle;
+            Origin = tractorBeam.Origin;
+            Texture = tractorBeam.Texture;
+            //End
+        }
+
+        public TractorBeam(Texture2D texture, float length)
+        {
+            Origin = new Vector2(0, texture.Height / 2f);
+            Texture = texture;
+            Length = length;
+        }
+    }
+}

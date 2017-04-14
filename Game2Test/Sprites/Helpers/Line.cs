0713c1525f0c21860fae6a76f660eeb1d3fcf86e
MechaKnightz <MechaKnightz@hotmail.com>
2017-04-14 14:49:35 +0200

    refactored some code in Line

--- Game2Test/Sprites/Helpers/Line.cs
+++ Game2Test/Sprites/Helpers/Line.cs
@@ -6,31 +6,31 @@ namespace Game2Test.Sprites.Helpers
 {
     public class Line
     {
-        public Vector2 p1, p2;
+        public Vector2 P1 { get; set; }
+        public Vector2 P2 { get; set; }
 
         public Line(Vector2 p1, Vector2 p2)
         {
-            this.p1 = p1;
-            this.p2 = p2;
+            this.P1 = p1;
+            this.P2 = p2;
         }
 
         public List<Vector2> GetPoints(int quantity)
         {
             var vectors = new List<Vector2>();
-            int ydiff = (int)p2.Y - (int)p1.Y, xdiff = (int)p2.X - (int)p1.X;
-            double slope = (double)(p2.Y - p1.Y) / (p2.X - p1.X);
-            double x, y;
+            int ydiff = (int)P2.Y - (int)P1.Y, xdiff = (int)P2.X - (int)P1.X;
+            var slope = (double)(P2.Y - P1.Y) / (P2.X - P1.X);
 
             --quantity;
 
             for (double i = 0; i < quantity; i++)
             {
-                y = slope == 0 ? 0 : ydiff * (i / quantity);
-                x = slope == 0 ? xdiff * (i / quantity) : y / slope;
-                vectors.Add(new Vector2((int)Math.Round(x) + p1.X, (int)Math.Round(y) + p1.Y));
+                var y = slope == 0 ? 0 : ydiff * (i / quantity);
+                var x = slope == 0 ? xdiff * (i / quantity) : y / slope;
+                vectors.Add(new Vector2((int)Math.Round(x) + P1.X, (int)Math.Round(y) + P1.Y));
             }
 
-            vectors.Add(p2);
+            vectors.Add(P2);
             return vectors;
         }
     }

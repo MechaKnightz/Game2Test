9e9a17b2644dfe636468331155af8945d6c3064f
MechaKnightz <MechaKnightz@hotmail.com>
2017-04-16 04:07:14 +0200

    final fix

--- Game2Test/Sprites/Entities/Turrets/BasicTurret.cs
+++ Game2Test/Sprites/Entities/Turrets/BasicTurret.cs
@@ -11,10 +11,25 @@ namespace Game2Test.Sprites.Entities.Turrets
         public BasicTurret(Texture2D texture, Vector2 position, Vector2 offset, float rotation, Shot shot, float energyCost, float turnRate, TurretType type, float cooldown) : base(texture, position, offset, rotation, shot, energyCost, turnRate, type, cooldown)
         {
         }
+        public BasicTurret(BaseTurret turret)
+        {
+            Offset = turret.Offset;
+            EnergyCost = turret.EnergyCost;
+            TurnRate = turret.TurnRate;
+            Type = turret.Type;
+            Cooldown = turret.Cooldown;
+            //Sprite
+            Rotation = turret.Rotation;
+            Position = turret.Position;
+            Rectangle = turret.Rectangle;
+            Origin = turret.Origin;
+            Texture = turret.Texture;
+            //End
+            Shot = new Shot(turret.Shot);
+        }
         public ITurret CloneTurret(ITurret turret)
         {
-            var tempTurret = new BasicTurret();
-            tempTurret = (BasicTurret) turret;
+            var tempTurret = new BasicTurret((BasicTurret)turret);
             return tempTurret;
         }
     }
--- Game2Test/Sprites/Entities/Turrets/LaserTurret.cs
+++ Game2Test/Sprites/Entities/Turrets/LaserTurret.cs
@@ -12,6 +12,23 @@ namespace Game2Test.Sprites.Entities.Turrets
         public LaserTurret(Texture2D texture, Vector2 position, Vector2 offset, float rotation, Shot shot, float energyCost, float turnRate, TurretType type, float cooldown, float length) : base(texture, position, offset, rotation, shot, energyCost, turnRate, type, cooldown)
         {
         }
+        public LaserTurret(LaserTurret turret)
+        {
+            Offset = turret.Offset;
+            EnergyCost = turret.EnergyCost;
+            TurnRate = turret.TurnRate;
+            Type = turret.Type;
+            Cooldown = turret.Cooldown;
+            Length = turret.Length;
+            //Sprite
+            Rotation = turret.Rotation;
+            Position = turret.Position;
+            Rectangle = turret.Rectangle;
+            Origin = turret.Origin;
+            Texture = turret.Texture;
+            //End
+            Shot = new Shot(turret.Shot);
+        }
         public ITurret CloneTurret(ITurret turret)
         {
             var tempTurret = new LaserTurret();
